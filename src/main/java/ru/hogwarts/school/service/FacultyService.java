86dae23ebf8cfc632d2c624440809f8882bb45c9
YuZu <yurkazubkov@yandex.ru>
2023-09-29 23:47:28 +0300

    added postgres

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -2,43 +2,48 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
-    private final Map<Long, Faculty> faculties = new HashMap<>();
-    private long lastId = 0;
+    private final FacultyRepository facultyRepository;
 
-    public FacultyService() {
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
     }
 
     public Faculty createFaculty(Faculty faculty) {
-        faculty.setId(++lastId);
-        faculties.put(lastId, faculty);
-        return faculty;
+        faculty.setId(0);
+        return facultyRepository.save(faculty);
     }
 
     public Faculty getFacultyById(long id) {
-        return faculties.get(id);
+        return facultyRepository.findById(id).orElse(null);
     }
 
     public Collection<Faculty> getAllFaculties() {
-        return faculties.values();
+        return facultyRepository.findAll();
     }
 
     public Faculty updateFaculty(Faculty faculty) {
-        return faculties.replace(faculty.getId(), faculty);
+        if (!facultyRepository.existsById(faculty.getId())) {
+            return null;
+        }
+        return facultyRepository.save(faculty);
     }
 
     public Faculty removeFaculty(long id) {
-        return faculties.remove(id);
+        Faculty faculty = facultyRepository.findById(id).orElse(null);
+        if (faculty == null) {
+            return null;
+        }
+        facultyRepository.delete(faculty);
+        return faculty;
     }
 
     public Collection<Faculty> getFacultiesByColor(String color) {
-        return faculties.values().stream().filter(faculty -> faculty.getColor().equals(color)).collect(Collectors.toList());
+        return facultyRepository.findByColor(color);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -2,43 +2,48 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
-    private final Map<Long, Student> students = new HashMap<>();
-    private long lastId = 0;
+    private final StudentRepository studentRepository;
 
-    public StudentService() {
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
     public Student createStudent(Student student) {
-        student.setId(++lastId);
-        students.put(lastId, student);
-        return student;
+        student.setId(0);
+        return studentRepository.save(student);
     }
 
     public Student getStudentById(long id) {
-        return students.get(id);
+        return studentRepository.findById(id).orElse(null);
     }
 
     public Collection<Student> getAllStudents() {
-        return students.values();
+        return studentRepository.findAll();
     }
 
     public Student updateStudent(Student student) {
-        return students.replace(student.getId(), student);
+        if (!studentRepository.existsById(student.getId())) {
+            return null;
+        }
+        return studentRepository.save(student);
     }
 
     public Student removeStudent(long id) {
-        return students.remove(id);
+        Student student = studentRepository.findById(id).orElse(null);
+        if (student == null) {
+            return null;
+        }
+        studentRepository.delete(student);
+        return student;
     }
 
     public Collection<Student> getStudentsByAge(int age) {
-        return students.values().stream().filter(student -> student.getAge() == age).collect(Collectors.toList());
+        return studentRepository.findByAge(age);
     }
 }
